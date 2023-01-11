3c5f969ab0105e90c8a991ad6f51a659a2c63650
Davy <dchuon@ucdavis.edu>
2023-01-11 09:15:58 -0800

    daily matchups dict populated for players vs team of current day

--- merge-stages.py
+++ merge-stages.py
@@ -9,6 +9,63 @@ import pandas as pd
 df1 = pd.read_csv("stage1-part3.csv")
 df2 = pd.read_csv("stage1-part1-stage2.csv")
 df3 = pd.read_csv("stage3.csv")
+df4 = pd.read_csv("daily-matchups.csv")
+
+
+playerName = df2['Player'].tolist()
+playerTeam = df2['Player Team'].tolist()
+homeTeam = df4['Home Team'].tolist()
+awayTeam = df4['Away Team'].tolist()
+dailyTeams = homeTeam + awayTeam
+
+homeTeamPlayers = []
+awayTeamPlayers = []
+
+playerInfo = list(zip(playerName, playerTeam))
+playerMatchups = dict()
+
+for player in playerInfo:
+  # print(player)
+  # print(player[0])
+  # print(player[1])
+  if player[1] not in playerMatchups:
+    playerMatchups[player[1]] = [player[0]]
+  else:
+    playerMatchups[player[1]].append(player[0])
+
+
+dailyMatchup = dict()
+
+
+for team in homeTeam:
+  if team in playerMatchups:
+    homeTeamPlayers.append(playerMatchups[team])
+
+  
+for team in awayTeam:
+  if team in playerMatchups:
+    awayTeamPlayers.append(playerMatchups[team])  
+
+
+
+for roster in homeTeamPlayers:
+  opposingTeam = awayTeam.pop(0)
+  dailyMatchup[opposingTeam] = roster
+
+for roster in awayTeamPlayers:
+  opposingTeam = homeTeam.pop(0)
+  dailyMatchup[opposingTeam] = roster
+
+
+print(dailyMatchup)
+
+
+# ! NOW MATCHUP HOMETEAMS VS AWAYTEAMPLAYERS AND AWAYTEAMS VS HOMETEAMPLAYERS
+
+# for i in homeTeamPlayers:
+#   print(i)
+
+
 
 df_final = df1.join(df2, how='right')
 
